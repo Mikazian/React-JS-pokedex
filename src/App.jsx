c71289958d374318fb374b13d8ec1ea86426b911
Mike Xiong <xiong.tchylon@gmail.com>
2023-09-19 12:21:06 +0200

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
 import "./App.css";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
+  const currentPokemon = pokemonList[0];
   return (
     // Affichage du composant
     <>
@@ -10,7 +22,11 @@ function App() {
         <MyTitle />
       </div>
       <div>
-        <PokemonCard />
+        <PokemonCard
+          pokemon={currentPokemon}
+          name={currentPokemon.name}
+          imgSrc={currentPokemon.imgSrc}
+        />
       </div>
     </>
   );
